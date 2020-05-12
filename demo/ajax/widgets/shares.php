7d742998c576758d8dcd7352f94480a75efaac5e
Blari <Blarior@gmail.com>
2020-05-12 18:51:43 +0300

    Create gh-pages

---
+++ demo/ajax/widgets/shares.php
@@ -0,0 +1,43 @@
+<?php sleep(2) ?>
+<div class="list-group list-group-lg">
+    <a href="#" class="list-group-item animated fadeInDown bg-warning-light">
+        <span class="thumb-sm float-left mr">
+            <img class="rounded-circle" src="../demo/img/people/a6.jpg" alt="...">
+        </span>
+        <i class="fa fa-circle float-right text-success mt-sm"></i>
+        <h6 class="no-margin"><strong>Jenny Wilington</strong></h6>
+        <small>just now</small>
+    </a>
+    <a href="#" class="list-group-item">
+        <span class="thumb-sm float-left mr">
+            <img class="rounded-circle" src="../demo/img/people/a1.jpg" alt="...">
+        </span>
+        <i class="fa fa-circle float-right text-danger mt-sm"></i>
+        <h6 class="no-margin">Maikel Basso</h6>
+        <small class="text-muted">about 2 mins ago</small>
+    </a>
+    <a href="#" class="list-group-item">
+        <span class="thumb-sm float-left mr">
+            <img class="rounded-circle" src="../demo/img/people/a2.jpg" alt="...">
+        </span>
+        <i class="fa fa-circle float-right text-info mt-sm"></i>
+        <h6 class="no-margin">Ianus Arendse</h6>
+        <small class="text-muted">about 42 mins ago</small>
+    </a>
+    <a href="#" class="list-group-item">
+        <span class="thumb-sm float-left mr">
+            <img class="rounded-circle" src="../demo/img/people/a3.jpg" alt="...">
+        </span>
+        <i class="fa fa-circle float-right text-success mt-sm"></i>
+        <h6 class="no-margin">Valdemar Landau</h6>
+        <small class="text-muted">one hour ago</small>
+    </a>
+    <a href="#" class="list-group-item mb-n-md">
+        <span class="thumb-sm float-left mr">
+            <img class="rounded-circle" src="../demo/img/people/a4.jpg" alt="...">
+        </span>
+        <i class="fa fa-circle float-right text-warning mt-sm"></i>
+        <h6 class="no-margin">Rick Teagan</h6>
+        <small class="text-muted">3 hours ago</small>
+    </a>
+</div>
---
+++ node_modules/line-awesome/README.md
@@ -0,0 +1,28 @@
+# Icons8 Line Awesome
+
+There is available a live [preview](https://icons8.com/line-awesome) of the icon set.
+
+
+## Good Boy License
+
+We’ve released the icon pack either under MIT or the [Good Boy License](https://icons8.com/good-boy-license/). We invented it. Please do _whatever your mom would approve of:_
+* Download
+* Change
+* Fork
+
+No tattoos!
+
+
+### Installing
+
+```shell
+npm install icons8/line-awesome
+```
+
+## Credits
+
+Based on the [Windows 10 icon pack](https://icons8.com/download-huge-windows8-set/). The original ones contains 4,500 icons and is too heavy for a single font. 
+
+## Questions or Ideas?
+
+If you have any questions or ideas about icons, [please feel free to contact us](https://github.com/icons8/line-awesome/issues).
